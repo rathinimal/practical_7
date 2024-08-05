1bef60e8904f792ceaf6e36edb57ec45d26d0ee3
rathinimal <rathinarasanimal@gmail.com>
2024-08-05 16:36:56 +0530

    Q1 and Q2 updated

--- Q1.scala
+++ Q1.scala
@@ -4,14 +4,12 @@ object even_Numbers {
         //declare given array numbers
         var num = List(1,2,3,4,5,6,7,8,9,10)
 
+        def filterEvenNumbers(numbers:List[Int]): List[Int] = {
+            numbers.filter(i=>i%2 ==0)
+        }
         val output = filterEvenNumbers(num)
         println(output)
         
-
-        def filterEvenNumbers(numbers:List[Int]): List[Int] = {
-            numbers.filter(i=>i%2 ==0)
-       
-        }
         
     }
 }
--- Q2.scala
+++ Q2.scala
@@ -3,9 +3,6 @@ object squ_numbers{
     def main(args:Array[String]): Unit ={
         val input_num = List(1,2,3,4,5)
 
-        val squareIt = (n:Int)=>n*n
-        val f = (op:(Int)=>Int,n:Int) => op(n)
-
         
         val square_numbers = input_num.map(i=>i*i)
         
